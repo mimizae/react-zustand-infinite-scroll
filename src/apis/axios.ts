516df66d39ad29279d4e70856a59bc011a88d203
mimizae <jim400934@gmail.com>
2026-01-09 21:04:59 +0900

    chore: 타임 아웃 시간 짧게 조정

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 10000,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
