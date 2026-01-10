35b07468b8c64275e8695460b8f423a7946fc1b5
mimizae <jim400934@gmail.com>
2026-01-10 19:56:52 +0900

    refact: 누락된 의존성 배열 추가

--- src/features/posts/post-list.tsx
+++ src/features/posts/post-list.tsx
@@ -21,7 +21,7 @@ export const PostList = () => {
     if (posts.length === 0) {
       loadPosts();
     }
-  }, []);
+  }, [loadPosts, posts.length]);
 
   return (
     <div className="mx-auto max-w-4xl px-4 py-8">
